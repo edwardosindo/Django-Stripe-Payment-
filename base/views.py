aa99d9c8d36192c770cf21f9b89d08175b4c4249
Edward Osindo <eddu.almasi@gmail.com>
2020-05-10 12:40:30 +0300

    first commit

---
+++ base/views.py
@@ -0,0 +1,37 @@
+from django.shortcuts import render, redirect
+from django.urls import reverse
+from django.http import JsonResponse
+
+import stripe
+stripe.api_key = "Your Secret Key"
+
+# Create your views here.
+
+def index(request):
+	return render(request, 'base/index.html')
+
+
+def charge(request):
+	amount = int(request.POST['amount'])
+	if request.method == 'POST':
+		print('Data:', request.POST)
+
+		customer = stripe.Customer.create(
+			email = request.POST['email'],
+			name = request.POST['nickname'],
+			source = request.POST['stripeToken'],
+		)
+
+		charge = stripe.Charge.create(
+			customer = customer,
+			amount = amount*100,
+			currency = 'kes',
+			description = "Donation",
+		)
+
+	return redirect(reverse('success', args=[amount]))
+
+
+def successMsg(request, args):
+	amount = args
+	return render(request, 'base/success.html', {'amount':amount})
